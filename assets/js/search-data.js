6cd239f70a21f0d19ec730f55c9a2e9dd30a22be
Alvin Voo <xoalvinox@gmail.com>
2019-10-28 17:49:28 +0800

    initial push

---
+++ assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"Ba ba black sheep have you any wool? ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2019/10/18/welcome-to-jekyll.html"
+      }]
