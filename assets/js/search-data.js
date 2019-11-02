83ae21b1071606b563fc3240e8afdab67c055a01
Alvin Voo <xoalvinox@gmail.com>
2019-11-02 13:10:01 +0800

    site update

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"Ba ba black sheep have you any wool? ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/2019/10/18/welcome-to-jekyll.html"
+        "title": "Straight to the point",
+        "excerpt":"It’s OK for one to be succinct and precise in saying what one has to say.Traditional education has taught us to ‘fill in the blanks’ or ‘write a 1000 words essay’ in which we had to conjure up extra words to get those few extra points.This creates a habit out...","categories": [],
+        "tags": ["ethics","efficiency"],
+        "url": "/2019/10/18/welcome-to-jekyll.html"
       }]
