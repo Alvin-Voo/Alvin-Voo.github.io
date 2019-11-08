bcee9032eb5bb25576748ef7e526b75e2141f045
Alvin Voo <xoalvinox@gmail.com>
2019-11-08 11:38:28 +0800

    updated

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Straight to the point",
-        "excerpt":"It’s OK for one to be succinct and precise in saying what one has to say.Traditional education has taught us to ‘fill in the blanks’ or ‘write a 1000 words essay’ in which we had to conjure up extra words to get those few extra points.This creates a habit out...","categories": [],
+        "excerpt":"It’s OK for one to be succinct and precise in saying what one has to say. Traditional education has taught us to ‘fill in the blanks’ or ‘write a 1000 words essay’ in which we had to conjure up extra words to get those few extra points. This creates a...","categories": [],
         "tags": ["ethics","efficiency"],
-        "url": "/2019/10/18/welcome-to-jekyll.html"
+        "url": "/2019/10/18/straight-to-the-point.html"
       }]
